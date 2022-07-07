24909c16c58681f805301335ff7b36e3eb0924bd
ramlaidi <83389072+ramlaidi@users.noreply.github.com>
2022-07-07 14:00:59 +0700

    Update merampok.js

--- plugins/merampok.js
+++ plugins/merampok.js
@@ -13,11 +13,11 @@ let nomors = m.sender
   let timers = clockString(_timers)
   let users = global.db.data.users
   if (new Date - global.db.data.users[m.sender].lastrob > 3600000){
-  if (10000 > users[who].money) throw 'Target Gaada Uang bodoh, Kismin dia'
+  if (10000 > users[who].money) throw 'Target Gaada Uang bodoh, Miskin Dia Kek Elu'
   users[who].money -= dapat * 1
   users[m.sender].money += dapat * 1
   global.db.data.users[m.sender].lastrob = new Date * 1
-  conn.reply(m.chat, `Berhasil Merampok Money Target Sebesar ${dapat}`, m)
+  conn.reply(m.chat, `Berhasil Merampok Money Target Sebesar 93`, m)
 
 }else conn.reply(m.chat, `Anda Sudah merampok dan berhasil sembunyi , tunggu ${timers} untuk merampok lagi`, m)
 }
