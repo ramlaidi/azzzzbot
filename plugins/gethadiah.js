8fbc9f5b699dcefb084ed1b78ed03ff5304489f7
ramlaidi <83389072+ramlaidi@users.noreply.github.com>
2022-07-08 17:04:30 +0700

    Update gethadiah.js

--- plugins/gethadiah.js
+++ plugins/gethadiah.js
@@ -8,7 +8,7 @@ global.db.data.users[m.sender].lastlink = new Date * 1
 let codeh = ['736390','882910','882010','715611','002819','562622','432282','322929','662522','443321','937100','762837','028393','625529','727638','992719','092739','727269','629461','239210']
 let getRandom = codeh[Math.floor(Math.random() * codeh.length)]
 conn.reply(m.chat, `
-https://ponselharian.com/iSHbYz
+https://cararegistrasi.com/71qEsQ
 
 Contoh:
 1. Harap klik link tersebut
